213e49371ba347cb039ec016d7b390f28174238a
FelipeRhoden <liperrhoden@gmail.com>
2019-12-17 22:52:47 -0300

    Primeiro teste de propriedades aleatorias

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+
+function random(num){
+    try {
+       const newNum =  Math.floor(Math.random() * num);
+       return newNum;
+
+    } catch(err){
+
+        console.error("Erro de Random numerico");
+    }
+}
+
 $.get("db.JSON",function(data,status){
-    alert("Data: " + data + "\nStatus: " + status);
+    if (status == "success"){
+
+        const ficha = JSON.stringify(data);
+        const cor = ficha.Coloracao[random(6)];
+        const caracteristica = ficha.Caracteristica[random(6)];
+        const ocupacao = ficha.Ocupacao[random(6)];
+
+        $("aparencia").text(cor + " e " + caracteristica);
+        $("ocupacao").text(ocupacao);
+    } else {
+        console.error("Erro de requisição");
+    }
 })
